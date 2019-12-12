176e58b19ead054435ff5ed4da8d56867a1076a3
thales.vieira <thales.vieira@dataprev.gov.br>
2019-12-12 18:14:08 -0300

    cleaning Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
@@ -23,13 +21,6 @@ function Header({ children, className, style }) {
 			<AppBar position='static'>
 				<Toolbar style={style} className={className}>
 					<Typography variant='h6' className={classes.brand}>
-						<IconButton
-							edge='start'
-							className={classes.menuButton}
-							color='inherit'
-							aria-label='menu'>
-							<MenuIcon />
-						</IconButton>
 						BOOSTER
 					</Typography>
 					{children}
