a529b322304e1e0a9d19b2e0f48212b154c37304
thaleshcv <thalesherbert@gmail.com>
2019-12-28 18:13:32 -0300

    removing unused imports

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Link as RouterLink, Redirect, Route, Switch } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 // UI components
